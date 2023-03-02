dc0dbe720495f3570239a94c582bc368957143c1
ThePepi1 <122203922+ThePepi1@users.noreply.github.com>
2023-03-02 11:07:35 +0100

    Project

---
+++ faces-train.py
@@ -0,0 +1,44 @@
+import os
+from PIL import Image
+import numpy
+import cv2
+face_Cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt2.xml')
+face_Cascade2 = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
+BASE_directory = os.path.dirname(os.path.abspath(__file__))
+images_directory = os.path.join(BASE_directory, 'imgs')
+
+current_id = 0
+pictures = []
+labels = {}
+labels_list = []
+
+for root , dirs, files  in os.walk(images_directory):
+    for file in files:
+        if file.endswith("png") or file.endswith("jpg"):
+            path = os.path.join(root, file)
+            label = os.path.basename(os.path.dirname(path)).replace(" ", "-").lower()
+            pil_img = Image.open(path).convert("L")
+            image_array = numpy.array(pil_img, "uint8")
+            if not label in labels:
+                labels[label] = current_id
+                current_id += 1
+            
+            id_ = labels[label]
+            faces = face_Cascade2.detectMultiScale(image_array, scaleFactor = 1.3, minNeighbors = 5)
+            print(faces)
+            for (x, y, w, h) in faces:
+                region_of_interests = image_array[y:y+h, x:x+w] 
+                pictures.append(region_of_interests)
+                labels_list.append(id_)
+
+
+
+while True:
+
+    cv2.imshow('Image', pictures[0])
+    if cv2.waitKey(1) == ord('q'):
+        break;
+
+
+print(labels_list)
+print(pictures)
---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+import cv2
+face_Cascade = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_alt.xml')
+face_Cascade2 = cv2.CascadeClassifier('cascades\data\haarcascade_frontalface_default.xml')
+
+
+
+wCam, hCam = 1080, 1200
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+while True:
+    ret, img = cap.read()
+    gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    #detecting a face (radius of interest)
+    faces = face_Cascade2.detectMultiScale(gray_img, scaleFactor = 1.2, minNeighbors = 5)
+    #showing face detection 
+    for (x, y, w, h,) in faces:
+        region_of_interests_grey = gray_img[y:y+h, x:x+w] 
+
+        #recognizing
+
+
+
+
+
+        #drawing rectangle
+        color = (255, 0, 0)
+        stroke = 2
+        end_coordinate_x = x + w
+        end_coordinate_y = y + h
+        cv2.rectangle(img, (x,y),(end_coordinate_x, end_coordinate_y), color)
+
+
+    #display video
+    cv2.imshow('Image', img)
+    if cv2.waitKey(1) == ord('q'):
+        break;
+
+cap.release()
+cv2.destroyAllWindows()
